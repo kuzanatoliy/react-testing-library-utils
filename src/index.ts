67d1e0c401f0072957e9d38bf6e5d33488fa11ce
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2022-07-24 20:14:20 +0300

    chore: add Interop constraintsprops

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import { TEST } from "./test/test";
+
+const temp = TEST.TEST1;
+
+if (temp === TEST.TEST1) {
+  console.log("bugaga");
+}
+
 // test
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,1 +1,8 @@
 // test
+export const enum TEST {
+  TEST1,
+  TEST2,
+  TEST3,
+}
+
+export const test1 = TEST.TEST1;
